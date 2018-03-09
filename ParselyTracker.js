14a0a6c1e297a4403006968f40f7e0376616865a
Alexander Lourenco <alexander.lourenco@gmail.com>
2018-03-09 15:37:50 +0100

    fix root url sending

--- ParselyTracker.js
+++ ParselyTracker.js
@@ -7,8 +7,9 @@ class ParselyTracker {
         this.apikey = apikey;
         // if you wanted to get a persistent uuid you could use the native Android or iOS methods for that (like android's getAdKey)
         this.parsely_site_uuid = uuidv4();
-        this.rootUrl = "http://srv.pixel.parsely.com/plogger/mobileproxy";
+        this.rootUrl = "http://srv.pixel.parsely.com/mobileproxy";
         this.os = Platform.OS;
+        this.urlref = "parsely_mobile_sdk";
         this.os_version = Platform.Version;
         this.manufacturer = Platform.OS === 'ios' ? "Apple" : "Android";
         this.appname = "Sample Parsely React App";
@@ -23,7 +24,8 @@ class ParselyTracker {
                 "idsite": this.apikey,
                 "manufacturer": this.manufacturer,
                 "appname": this.appname,
-                "parsely_site_uuid": this.parsely_site_uuid
+                "parsely_site_uuid": this.parsely_site_uuid,
+                "urlref": this.urlref
             },
             "events": [
                 {
@@ -37,7 +39,13 @@ class ParselyTracker {
            headers: {
                "Content-Type": "application/x-www-form-urlencoded"
            },
-            body: JSON.stringify(params)
+            body:
+                JSON.stringify(
+                    {
+                        "rqs": params
+                    }
+
+            )
         }).then(
             (response) => console.log(response)
         ).catch(
